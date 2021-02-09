9fdd5e3feba3dae55abb94ba5881f2abc91a7fd3
Kirill Shilin <kvshilin@yandex.ru>
2021-02-10 01:29:21 +0300

    Second try

--- pages/script.js
+++ pages/script.js
@@ -1,12 +1,12 @@
-let editButton = document.querySelector('.profile__info_edit-button');
+let editButton = document.querySelector('.profile__info__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = document.querySelector('.form-container__close-button');
 let submitButton = document.querySelector('.form-container__submit');
 let formElement = popup.querySelector('.form-container')
 let nameInput = document.querySelector('.form-container__name-input');
-let nameField = document.querySelector('.profile__info_name');
+let nameField = document.querySelector('.profile__info__name');
 let jobInput = document.querySelector('.form-container__job-input');
-let jobField = document.querySelector('.profile__info_job');
+let jobField = document.querySelector('.profile__info__job');
 
 function togglePopup() {
   popup.classList.toggle('popup_opened')
