6b4845402f31e10b12d07683979fdacebb1baab2
Kirill Shilin <kvshilin@yandex.ru>
2021-02-13 18:03:23 +0300

    commiting

--- pages/script.js
+++ pages/script.js
@@ -1,14 +1,21 @@
 let editButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close-button');
+let popupNewPlace = document.querySelector('#new-place')
+let closeButton = document.querySelectorAll('.popup__close-button');
 let formElement = popup.querySelector('.form-container')
 let nameInput = document.querySelector('.form-container__input_name');
 let nameField = document.querySelector('.profile__name');
 let jobInput = document.querySelector('.form-container__input_job');
 let jobField = document.querySelector('.profile__job');
-let likeButton = document.querySelectorAll('.card__like')
+let placeInput = document.querySelector('.form-container__input_place');
+let imageInput = document.querySelector('.form-container__input_image');
+let likeButton = document.querySelector('.card__like');
+let addButton = document.querySelector('.profile__add-button');
+let cardTemplate = document.querySelector('#card-template').content;
+let elementsList = document.querySelector('.elements');
 
-function openPopup() {
+
+function openPopup(evt) {
   nameInput.value = nameField.textContent
   jobInput.value = jobField.textContent
   popup.classList.add('popup_opened')
@@ -20,22 +27,79 @@ function closePopup(evt) {
   }
 }
 
-function toggleLike (evt) {
-  evt.target.classList.toggle('card__like_active')
-}
-
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
   nameField.textContent = nameInput.value;
   jobField.textContent = jobInput.value;
-
-  // Марина, вас нет в слаке, поэтому пишу здесь - если я просто заменю на closePopup, то у меня форма не будет сабмититься. 
-  // Просто повесить еще одно условие на сабмит кнопку не работает. Как написать функцию Popup так, чтобы сабмитилась, я не разобрался...
+  
   popup.classList.remove('popup_opened');
 }
 
 editButton.addEventListener('click', openPopup)
-closeButton.addEventListener('click', closePopup)
+addButton.addEventListener('click', () => {
+  popupNewPlace.classList.add('popup_opened')
+})
+closeButton.forEach(button => {
+  button.addEventListener('click', closePopup)
+})
 popup.addEventListener('click', closePopup)
-likeButton.addEventListener('click', toggleLike)
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+initialCards.forEach((element) => {
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__like').addEventListener("click", (evt) => {
+      evt.target.classList.toggle('card__like_active')
+  })
+  cardElement.querySelector('.card__delete-button').addEventListener("click", (evt) => {
+    evt.target.closest('.card').remove();
+  })
+  cardElement.querySelector('.image-container__image').src = element.link;
+  cardElement.querySelector('.image-container__title').textContent = element.name;
+  cardElement.querySelector('.popup__close-button').addEventListener("click", (evt) => {
+    evt.target.closest('.card').querySelector('.popup').classList.remove('popup_opened')
+})
+  cardElement.querySelector('.popup').addEventListener("click", (evt) => {
+    evt.target.classList.toggle('popup_opened')
+  })
+
+  imageElement = cardElement.querySelector('.card__image')
+  imageElement.addEventListener("click", evt => {
+    evt.target.closest('.card').querySelector('.popup').classList.add('popup_opened')
+  })
+  
+  elementsList.append(cardElement)
+})
