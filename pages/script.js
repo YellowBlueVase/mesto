de3036f167f4ea7f3b41dac56a651c464d321b5a
Kirill Shilin <kvshilin@yandex.ru>
2021-02-14 03:39:04 +0300

    Feat: Ready for second review

--- pages/script.js
+++ pages/script.js
@@ -1,100 +1,72 @@
 // GLOBAL VARIABLES AND LISTS -------------------------------------------------------------------------------
 
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let popupNewPlace = document.querySelector('#new-place')
-let closeButton = document.querySelectorAll('.popup__close-button');
-let formElement = popup.querySelector('.form-container')
-let imageContainer = document.querySelector('#new-place-container')
-let nameInput = document.querySelector('.form-container__input_name');
-let nameField = document.querySelector('.profile__name');
-let jobInput = document.querySelector('.form-container__input_job');
-let jobField = document.querySelector('.profile__job');
-let avatarInput = document.querySelector('.form-container__input_avatar');
-let avatarField = document.querySelector('.profile__avatar')
-let placeInput = document.querySelector('.form-container__input_place');
-let imageInput = document.querySelector('.form-container__input_image');
-let likeButton = document.querySelector('.card__like');
-let addButton = document.querySelector('.profile__add-button');
-let cardTemplate = document.querySelector('#card-template').content;
-let elementsList = document.querySelector('.elements');
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-]; 
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button');
+
+const popups = document.querySelectorAll('.popup');
+
+const popupCloseButtons = document.querySelectorAll('.popup__close-button')
+
+const editForm = document.querySelector('.form-container_edit-profile');
+const addNewPlaceForm = document.querySelector('.form-container_new-place');
+
+const nameInput = document.querySelector('.form-container__input_name');
+const nameField = document.querySelector('.profile__name');
+const jobInput = document.querySelector('.form-container__input_job');
+const jobField = document.querySelector('.profile__job');
+const placeInput = document.querySelector('.form-container__input_place');
+const imageInput = document.querySelector('.form-container__input_image');
+const likeButton = document.querySelector('.card__like');
+const cardTemplate = document.querySelector('#card-template').content;
+const elementsList = document.querySelector('.elements');
+
 
 // FUNCTIONS -------------------------------------------------------------------------------  
-function formEditSetUp() {
+function openPopup(popup_class) {
+  document.querySelector(popup_class).classList.add('popup_opened')
+  if (popup_class === '.popup_type_edit-profile') {
+    nameInput.value = nameField.textContent;
+    jobInput.value = jobField.textContent
+  }
+}
+
+function closePopup(evt) {
+  if (evt.target.classList.contains('popup') || evt.target.classList.contains('popup__close-button') || evt.target.classList.contains('form-container__submit')) {
+    evt.target.closest('.popup').classList.remove('popup_opened')
+  }
+}
+
+function editProfile (evt) {
+  evt.preventDefault(); 
+  openPopup('.popup_type_edit-profile')
   nameInput.value = nameField.textContent;
-  jobInput.value = jobField.textContent
+  jobInput.value = jobField.textContent;
 
-  let popupEdit = document.querySelector('#edit-profile')
-  popupEdit.classList.add('popup_opened')
-  let closeButton = popupEdit.querySelector('.popup__close-button')
-  popupEdit.addEventListener('click', (evt) => {
-    if (evt.target === (popupEdit) || evt.target === closeButton) {
-      popupEdit.classList.remove('popup_opened')
-    }
-  })
-  formElement.addEventListener('submit', formSubmitHandler)
+  editForm.addEventListener('submit', submitEditForm)
 }
 
-function formAddNewPlace() {  
-  let popupNewPlace = document.querySelector('#new-place')
-  popupNewPlace.classList.add('popup_opened')
-  let closeButton = popupNewPlace.querySelector('.popup__close-button')
-  popupNewPlace.addEventListener('click', (evt) => {
-    if (evt.target === (popupNewPlace) || evt.target === closeButton) {
-      popupNewPlace.classList.remove('popup_opened')
-    }
-  })
-  imageContainer.addEventListener('submit', formSubmitNewPlace)
-}
-
-function formSubmitHandler (evt) {
+function submitEditForm (evt) {
   evt.preventDefault(); 
   nameField.textContent = nameInput.value;
   jobField.textContent = jobInput.value;
-  if (avatarInput.value) {
-    avatarField.src = avatarInput.value
-  }
-  
-  popup.classList.remove('popup_opened');
+  closePopup(evt)
 }
 
-function formSubmitNewPlace (evt) {
-  evt.preventDefault();
+function addNewPlace (evt) {
+  evt.preventDefault();  
+  openPopup('.popup_type_new-place')
+  addNewPlaceForm.addEventListener('submit', submitNewPlaceForm)
+}
 
+function submitNewPlaceForm (evt) {
+  evt.preventDefault();
   const newPlace = {
     name: placeInput.value,
     link: imageInput.value
   }
 
-  createNewCard(newPlace)
-
-  evt.target.closest('.popup').classList.remove('popup_opened')
+  elementsList.prepend(createNewCard(newPlace))
+  closePopup(evt)
 
   placeInput.value = ''
   imageInput.value = ''
@@ -111,28 +83,24 @@ function createNewCard(element) {
   cardElement.querySelector('.card__delete-button').addEventListener("click", (evt) => {
     evt.target.closest('.card').remove();
   })
-  
-  imageElement = cardElement.querySelector('.card__image')
-  imageElement.addEventListener("click", evt => {
-    evt.target.closest('.card').querySelector('.popup').classList.add('popup_opened')
+
+  cardElement.querySelector('.card__image').addEventListener("click", () => {
+    openPopup('.popup_type_large-image')
+    document.querySelector('.image-container__image').src = element.link;
+    document.querySelector('.image-container__title').textContent = element.name;
   })
-  cardElement.querySelector('.image-container__image').src = element.link;
-  cardElement.querySelector('.image-container__title').textContent = element.name;
-  cardElement.querySelector('.popup__close-button').addEventListener("click", (evt) => {
-    evt.target.closest('.card').querySelector('.popup').classList.remove('popup_opened')
-})
-  cardElement.querySelector('.popup').addEventListener("click", (evt) => {
-    evt.target.classList.toggle('popup_opened')
-  })
-  
-  elementsList.prepend(cardElement)
+  return cardElement
 }
 
 // EXECUTION COMMANDS -------------------------------------------------------------------------------
-
-editButton.addEventListener('click', formEditSetUp)
-addButton.addEventListener('click', formAddNewPlace)
-
 initialCards.forEach((item) => {
-  createNewCard(item)
+  elementsList.prepend(createNewCard(item))
+})
+editButton.addEventListener('click', editProfile)
+addButton.addEventListener('click', addNewPlace)
+popupCloseButtons.forEach(button => {
+  button.addEventListener('click', closePopup)
+})
+popups.forEach(popup => {
+  popup.addEventListener('click', closePopup)
 })
